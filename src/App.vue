401d0f5ad3ac65e2beac7bfb0be8e7238818749c
Hasin Hayder <countdraculla@gmail.com>
2023-08-10 22:07:36 +0600

    Episode 06 Code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
 <script setup>
-
+const posts=[
+  {
+    title: "Deadbeef Decrementation",
+    excerpt: "How to decrement from a magic debug value to zero.",
+    tags: ["spirituality"],
+  },
+  {
+    title: "Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater",
+    excerpt: "♫ Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater ♫",
+    tags: ["cats"],
+  },
+  {
+    title: "Why My Practice of Thelema Makes Me Better at World of Warcraft",
+    excerpt: "\"Do what thou wilt\" shall be the whole of the Law.",
+    tags: ["enchiladas"],
+  },
+  {
+    title:'Shrek Predicted the COVID-19 Pandemic',
+    excerpt:'Find out how Shrek is disrupting astrology.',
+    tags:[{
+      name:'orcas',
+      slug:'orcas',
+      color:'blue'
+    },{
+      name:'beer',
+      slug:'beer',
+      color:'red'
+    }]
+  },
+  {
+    title:'Lorem Ipsum',
+    excerpt:'You know what time it is: lorem ipsum time.',
+    tags:['stoicism']
+  }
+]
 </script>
 
 <template>
@@ -156,14 +190,5 @@
 </template>
 
 <style scoped>
-input[type=checkbox]:checked+label span:first-of-type {
-  background-color: #10B981;
-  border-color: #10B981;
-  color: #fff;
-}
 
-input[type=checkbox]:checked+label span:nth-of-type(2) {
-  text-decoration: line-through;
-  color: #9CA3AF;
-}
 </style>
