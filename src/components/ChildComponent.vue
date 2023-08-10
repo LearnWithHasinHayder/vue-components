16ef6920b6ed59579386954536b3a38774827b4d
Hasin Hayder <countdraculla@gmail.com>
2023-08-10 22:20:26 +0600

    Episode 08 Code

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+    <h2 class="my-2 text-xl">Child Component</h2>
+</template>
+<style></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ChildComponent from './ChildComponent.vue';
+</script>
+<template>
+    <h1 class="my-5 text-2xl">Parent Component</h1>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+</template>
